0126e3a32edac24dc458c82b66634874d947c69f
Gungcakra <agungcakra888@gmail.com>
2025-01-06 10:23:50 +0800

    Implement Post resource with CRUD functionality and update Home page to display posts

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,34 @@
-export default function Home({name}) {
+import { Link } from "@inertiajs/react";
+
+export default function Home({posts, name}) {
+    console.log(posts);
     return (
         <>
 
             <h1 className="title">Hello {name}</h1>
+
+            <div className="">
+                {posts.data.map((post) => (
+                    <div className="p-4 border-b" key={post.id} > 
+                        <div className="text-sm text-slate-600">
+                            <span>Posted On :</span>
+                            <span>{new Date(post.created_at).toLocaleTimeString()}</span>
+                        </div>
+                        <p className="font-medium">{post.body}</p>
+                    </div>
+                    ))}
+            </div>
+            <div className="py-12 px-4">
+                {posts.links.map((link) => (
+                    link.url ? (
+                    <Link href={link.url} key={link.label} dangerouslySetInnerHTML={{ __html:link.label }} className={`p-1 mx-1 ${link.active ? "text-blue-500 font-bold" : ""}`}/>
+                    ) : (
+                    <span key={link.label} dangerouslySetInnerHTML={{ __html:link.label }} className="p-1 mx-1 text-slate-400"></span>
+                    )
+                ))}
+            </div>
         </>
     
     );
 };
+
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render( 'Home',[
-        'name' => 'John Doe',
-    ]);
-});
+Route::get('/',[PostController::class, 'index']);
 
-// Route::inertia('/','Home');
+Route::resource('posts', PostController::class)->except('index');
+
